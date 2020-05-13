1d491ffa848608c534ad1ed9f2e6d3ddd81a4686
himanshu9345 <hphimanshu6@gmail.com>
2020-05-12 21:53:47 -0400

    addded Minimum Time to Collect All Apples in a Tree

--- Binary Tree/1443. Minimum Time to Collect All Apples in a Tree.py
+++ Binary Tree/1443. Minimum Time to Collect All Apples in a Tree.py
@@ -1,45 +1,33 @@
-# class Solution(object):
-#     walk=0
-#     def minTime(self, n, edges, hasApple):
-#         dict1={}
-#         for i,j in edges:
-#             if i in dict1:
-#                 dict1[i].append(j)
-#             else:
-#                 dict1[i]=[j]
+class Solution(object):
+    walk=0
+    def minTime(self, n, edges, hasApple):
+        dict1 = {}
+        for i, j in edges:
+            if i in dict1:
+                dict1[i].append(j)
+            else:
+                dict1[i]=[j]
         
-#         def dfs(dict1,root,dist,apple):
-#             print (root,dist,self.walk)
-#             if root not in dict1:
-#                 return apple
-#             flag=True
-#             apple=False
-#             for i in dict1[root]:
-#                 print i,hasApple[i],root,flag,self.walk
-#                 if hasApple[i]:
-#                     if flag:
-#                         self.walk+=dist
-#                         flag=False
-#                     else:
-#                         self.walk+=2
-#                     dist=0
-#                     apple=True
+        def dfs(dict1,root,dist):
+            #return if any apple were found in this edge
+            if root in dict1:
                 
-#                 val = dfs(dict1,i,dist+2,apple)
-#                 if val or apple:
-#                     apple=True
-#                 if apple:
-#                     flag=False
-#             return apple
-#         dfs(dict1,0,2,False)
-#         return self.walk
-                    
+                dist=0
+                for i in dict1[root]:
+                    if hasApple[i]:
+                        # print(i,"add")
+                        dist+=2
+                    subtreewalk = dfs(dict1,i,0)
+                    if subtreewalk>0 and not hasApple[i]:
+                        # print(i,"add1",subtreewalk)
+                        subtreewalk+=2
+                    dist+=subtreewalk
+                    # print(dist,i)
+                if dist==0:
+                    return 0
+                return dist
+            return 0
+                        
+        return dfs(dict1,0,0)
 
-    
-
-
-
-       
-       
         
-       
