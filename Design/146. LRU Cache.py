cdc0c9d8b7b3d19ec9019475a10b20a46e824edf
himanshu9345 <hbp53@njit.edu>
2020-05-15 16:31:15 -0400

    added new soln in lrucache

--- Design/146. LRU Cache.py
+++ Design/146. LRU Cache.py
@@ -1,3 +1,4 @@
+# 206 ms
 class Node(object):
     def __init__(self, key, val):
         self.prev = None
@@ -84,4 +85,64 @@ class LRUCache(object):
         if not self.end:
             self.end = new_node
         return new_node
+# 180 ms
+'''
+#add#
+we any key is visited then its count get incremented.
+and key is appended to queue.
+#remove#
+when cache size inc then elemented are emoved from queue 
+and key whos counter resset to zero is the key to be removed.
+'''
+class LRUCache(object):
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.size = 0
+        self.cache = dict()
+        self.visited = dict()
+        self.queue = deque()
         
+
+    def get(self, key):
+        if key in self.cache:
+            self.queue.append(key)
+            self.visited[key] += 1 
+            return self.cache[key]
+        return -1
+                  
+
+    def put(self, key, value):
+        if key in self.cache:
+            self.cache[key] = value 
+            self.queue.append(key)
+            self.visited[key] += 1 
+            return 
+        
+        self.cache[key] = value 
+        self.visited[key] = 1
+        self.queue.append(key)
+        self.size += 1 
+        
+        if self.size > self.capacity:
+            self.remove()
+        return         
+    
+    
+    def remove(self):
+        
+        while self.queue:
+            cur = self.queue.popleft()
+            self.visited[cur] -= 1 
+            if self.visited[cur] == 0:
+                self.size -= 1 
+                del self.cache[cur]
+                # print(self.cache)
+                return 
+
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
