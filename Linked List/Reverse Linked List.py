0a7500e94d855cfdc08288caadf31e747126f204
himanshu9345 <hbp53@njit.edu>
2020-06-05 20:28:59 -0400

    reversed linkedlist with recursion

--- Linked List/Reverse Linked List.py
+++ Linked List/Reverse Linked List.py
@@ -7,3 +7,35 @@ def reverseList(self, head):
         next1.next = head
         head = next1
     return head
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+#         head1=head
+        
+#         while(head1 and head1.next):
+#             next1=head1.next
+#             head1.next=head1.next.next
+#             next1.next=head
+#             head=next1
+#         return head
+        def helper(head):
+            if not head or not head.next:
+                return head
+            p = helper(head.next)
+            head.next.next = head
+            head.next  = None
+            return p
+                        
+    
+        return helper(head) 
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        
