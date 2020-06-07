dfb5dc44a548e2aa5218a8d545b9d04c66659d01
himanshu9345 <hbp53@njit.edu>
2020-06-07 15:32:04 -0400

    added iterative apporach for merge sort

--- Array and String/Sort an Array.py
+++ Array and String/Sort an Array.py
@@ -28,4 +28,48 @@ class Solution(object):# recursive top to bottom
         :type nums: List[int]
         :rtype: List[int]
         """
+class Solution(object):# iterative approach
+    def merge(self, list1, list2):
+        left = 0
+        right = 0
+        res = []
+        while left < len(list1) and right < len(list2):
+            if list1[left] > list2[right]:
+                res.append(list2[right])
+                right += 1
+            else:
+                res.append(list1[left])
+                left += 1
+        
+        res.extend(list1[left:])
+        res.extend(list2[right:])
+        return res
+    
+    def sortArray(self, nums):
+        
+        
+        if len(nums) == 1:
+            return nums
+        
+        nums = [[i] for i in nums]
+        while len(nums) != 1:
+            res = []
+            for i in range(len(nums)/2):
+                res.append(self.merge(nums[2*i], nums[2*i+1]))
+            if len(nums)%2 == 1:
+                res.append(nums[-1])
+            # print res
+            nums = res
+        return nums[0]
+                    
+            
+#         mid = len(nums)/2
+#         left = self.sortArray(nums[:mid])
+#         right = self.sortArray(nums[mid:])
+#         return self.merge(left, right)
+        
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
         
