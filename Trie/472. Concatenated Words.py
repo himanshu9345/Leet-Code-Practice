1918c9cafd4e26f505eef76a7371308e7b45bf37
himanshu9345 <hbp53@njit.edu>
2020-06-09 16:30:38 -0400

    new approach to solve Concatenated Words

--- Trie/472. Concatenated Words.py
+++ Trie/472. Concatenated Words.py
@@ -52,4 +52,21 @@ class Solution(object):
         :type words: List[str]
         :rtype: List[str]
         """
-        
+class Solution(object):
+    def findAllConcatenatedWordsInADict(self, words):
+        seen = set(words)
+        def helper(word, dict1):
+            if word in dict1:
+                return True
+            for i in range(len(word)):
+                if word[i:] in dict1 and helper(word[:i], dict1):
+                    return True
+            
+            return False
+        res = []
+        for i in words:
+            seen.remove(i)
+            if helper(i, seen):
+                res.append(i)
+            seen.add(i)
+        return res
