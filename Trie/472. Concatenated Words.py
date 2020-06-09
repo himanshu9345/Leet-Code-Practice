c58d6fd38fe27fc319683ad3586e1658831df8e7
himanshu9345 <hbp53@njit.edu>
2020-06-09 16:17:17 -0400

    Concatenated Words with trie and backtracking

---
+++ Trie/472. Concatenated Words.py
@@ -0,0 +1,55 @@
+class Node(object):
+    def __init__(self):
+        self.child = {}
+        self.end = False
+class Solution(object):
+    def findAllConcatenatedWordsInADict(self, words):
+        if not words:
+            return words
+        root = Node()
+        for i in words:
+            node  = root
+            for k in i:
+                if k not in node.child:
+                    node.child[k] = Node()
+                node = node.child[k]
+            node.end = True
+        res = []
+        head = root
+        def helper(word, end, found, root):
+            # print word, end, found, root.child, root.end
+            if not word:
+                if end and found>=1:
+                    return True
+                return False
+            
+            isTrue = False
+            start = word[0]
+            
+            if start not in root.child:
+                if start in head.child and end:
+                        return helper(word[1:], head.child[start].end, found + 1, head.child[start])
+                return False
+            else:
+                if end:
+                    if start in head.child:
+                        isTrue = isTrue or helper(word[1:], head.child[start].end, found + 1, head.child[start])
+                isTrue = isTrue or helper(word[1:], root.child[start].end, found, root.child[start])
+
+            return isTrue
+                
+            
+        
+        for i in words:
+            if i !="" and i[0] in root.child:
+                if helper(i[1:], root.child[i[0]].end, 0, root.child[i[0]]):
+                    res.append(i)
+        
+        return res
+                
+        
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        
